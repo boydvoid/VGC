5b4ca54ecc39c36b7a248106b86494b91460b802
Bobby Boyd <bboyd2008@gmail.com>
2019-02-11 12:30:19 -0800

    Added Register to userController

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,5 +1,8 @@
 const db = require('../Models');
 const passport = require('passport');
+const saltRounds = 10;
+const bcrypt = require('bcrypt');
+ 
 
 module.exports = {
   checkLogin: (req, res) => {
@@ -14,5 +17,49 @@ module.exports = {
     }).then(userInfo => {
       res.send(userInfo)
     })
+  },
+  createUser: (req, res) => {
+    
+    req.checkBody('username', 'Username cannot be empty.').notEmpty();
+    req.checkBody('email', 'Email field must not be empty.').notEmpty();
+    req.checkBody('email', 'Email field must be and email.').isEmail();
+    req.checkBody('password', 'Password must be 8 characters long.').len(8, 100);
+    req.checkBody('passwordMatch', 'Password must be 8 characters long.').len(8, 100);
+    req.checkBody('passwordMatch', 'Password must be 8 characters long.').equals(req.body.password);
+    
+    var errors = req.validationErrors();
+
+    if (errors) {
+      res.send(errors)
+
+    } else {
+
+      //hash the password 
+      var salt = bcrypt.genSaltSync(saltRounds);
+      var hash = bcrypt.hashSync(req.body.password, salt);
+      //bcrypt the password then insert 
+    
+      db.users.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        theme: 0
+      })
+      .then((created) => {
+          req.login(created._id, (err) => {
+              res.send(true)
+          })
+      .catch(err => {
+        //create user errors
+        //either duplicate username or email
+        if(err){
+          console.log(err.errmsg)
+          let data = [err.errmsg]
+          res.send(data) 
+        }
+      })
+    })
   }
+  }
+
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,6 @@
-var passport = require('passport');
-var bcrypt = require('bcrypt');
-let db = require('../Models');
-let saltRounds = 10;
 const router = require("express").Router();
 const usersController = require('../Controllers/usersController');
+const passport = require('passport');
 
 router
   .route("/checkLogin")
@@ -25,47 +22,9 @@ router.get('/logout', function (req, res) {
   res.send(false)
 });
 
-// Create a new user
-router.post("/register", function (req, res) {
-  req.checkBody('username', 'Username cannot be empty.').notEmpty();
-  req.checkBody('email', 'Email field must not be empty.').notEmpty();
-  req.checkBody('email', 'Email field must be and email.').isEmail();
-  req.checkBody('password', 'Password must be 8 characters long.').len(8, 100);
-  req.checkBody('passwordMatch', 'Password must be 8 characters long.').len(8, 100);
-  req.checkBody('passwordMatch', 'Password must be 8 characters long.').equals(req.body.password);
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-
-    res.send(errors)
-  } else {
-    //hash the password 
-    var salt = bcrypt.genSaltSync(saltRounds);
-    var hash = bcrypt.hashSync(req.body.password, salt);
-    //bcrypt the password then insert
-
-        db.users.create({
-          username: req.body.username,
-          email: req.body.email,
-          password: hash,
-          theme: 0
-        }).then((created) => {
-            req.login(created._id, (err) => {
-                res.send(true)
-            })
-        })
-        .catch(err => {
-          //create user errors
-          //either duplicate username or email
-          console.log(err.errmsg)
-          let data = [err.errmsg]
-          res.send(data) 
-        })
-     
-  }
-});
-
+router
+  .route("/register")
+  .post(usersController.createUser)
 
 //req.login uses these functions 
 passport.serializeUser(function (user_id, done) {
@@ -78,14 +37,4 @@ passport.deserializeUser(function (user_id, done) {
 module.exports = router;
 
 
-function checkForMultipleUsers(req) {
-  let userId;
-
-  //check if req.user is an object, if there are multiple users in a table than it is, if there's one than it's not
-  if (typeof req.user !== 'object') {
-    return userId = req.user
-  } else {
-    return userId = req.user.id
-  }
-}
 
