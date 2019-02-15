62170b5bf26c9fc75434158d1b4aa1d310aa8fcb
unknown <41927843+rvanhorn@users.noreply.github.com>
2019-02-14 19:32:10 -0800

    Fix API Cover bug, random React errors.

--- client/src/Components/Searchbar/Searchbar.js
+++ client/src/Components/Searchbar/Searchbar.js
@@ -4,19 +4,19 @@ import Button from '../Button/Button';
 import './Searchbar.css'
 const Searchbar = (props) => (
 
-  <nav class="searchbar">
+  <nav className="searchbar">
     <div>
     </div>
     <span className="ml-auto">
       <ul>
         <li>
-      <Button text={<i class="fas fa-search"></i>} class="search-btn" onclick={props.openRightPanel}/>
+      <Button text={<i className="fas fa-search"></i>} class="search-btn" onclick={props.openRightPanel}/>
         </li>
         <li>
           <p>Night Mode</p>
         </li>
         <li>
-          <input type="checkbox" id="switch" onChange={props.toggleTheme} /><label for="switch" >Toggle</label>
+          <input type="checkbox" id="switch" onChange={props.toggleTheme} /><label htmlFor="switch" >Toggle</label>
         </li>
       </ul>
     </span>
--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import userAPI from '../../utils/userAPI';
 
 import gamesAPI from '../../utils/gamesAPI';
@@ -7,189 +7,182 @@ import Searchbar from '../../Components/Searchbar/Searchbar';
 import './Dashboard.css';
 import RightPanel from '../../Components/RightPanel/RightPanel';
 import Chat from '../../Components/Chat/Chat'
-import { Socket } from 'net';
+
 class Dashboard extends Component {
 
-  state = {
+	state = {
 
-    theme: this.props.theme,
-    searchedGames: [],
-    rightPanelOpen: false,
-    chatboxExpanded: false,
-    socket: this.props.socket
-  }
+		theme: this.props.theme,
+		searchedGames: [],
+		rightPanelOpen: false,
+		chatboxExpanded: false,
+		socket: this.props.socket
+	};
 
 	componentDidMount = () => {
-    this.switchState();
-  };
+		this.switchState();
+	};
 
-	getGame = () => {
+	logout = () => {
+		userAPI.logout().then(() => {
+			//reload the window on sucessful logout
+			window.location.reload();
+		});
+	};
+
+	//check the state of the theme toggle on the dashboard
+	switchState = () => {
+		if (this.props.theme === 1) {
+			document.getElementById("switch").checked = false;
+		} else if (this.props.theme === 2) {
+			document.getElementById("switch").checked = true;
+		}
+	};
 
 
-		gamesAPI.gameID("19560").then((data) => {
-				console.log(data);
+	toggleTheme = () => {
+		if (this.state.theme === 1) {
+			let data = {
+				theme: 2
+			};
+			userAPI.update(data).then(() => {
+				this.setState({
+					theme: 2
+				});
+
+				document.getElementById("theme-div").classList.remove("light-theme");
+				document.getElementById("theme-div").classList.add("dark-theme")
 			})
+		} else {
+
+			let data = {
+				theme: 1
+			};
+			userAPI.update(data).then(() => {
+				this.setState({
+					theme: 1
+				})
+			});
+			document.getElementById("theme-div").classList.remove("dark-theme");
+			document.getElementById("theme-div").classList.add("light-theme")
+		}
+	};
+
+	openRightPanel = () => {
+		document.getElementById("mySidenav").style.width = "900px";
+		this.setState({
+			rightPanelOpen: true,
+			searchedGames: []
+		})
+	};
+
+	closeRightPanel = () => {
+		document.getElementById("mySidenav").style.width = "0";
+		this.setState({
+			rightPanelOpen: false,
+			searchedGames: []
+		})
+	};
+
+	gameSearch = (event) => {
+		event.preventDefault();
+		if (this.state.rightPanelOpen === true) {
+
+			let query = document.getElementById("gameSearch").value;
+			gamesAPI.gameSearch(query).then(data => {
+				// this.setState({
+				//   searchedGames: data
+				// })
+				console.log(data.data);
+				//data.data[0].game.cover
+				//get cover
+				if (this.state.rightPanelOpen === true) {
+
+					data.data.forEach(element => {
+
+						this.coverSearch(element)
+
+					});
+				}
+			})
+		}
+	};
+
+	coverSearch = (query) => {
+		//need to send game name and id to put in state
+		if (query.game !== undefined && query.game.cover !== undefined) {
+
+			gamesAPI.gameCover(query.game.cover).then(data => {
+				let tempArray = this.state.searchedGames;
+				if (data.data !== undefined && data.data[0].url !== undefined) {
+
+					data.data[0].url = data.data[0].url.replace('t_thumb', 't_1080p');
+					tempArray.push(
+						{
+							id: query.game.id,
+							imgUrl: data.data[0].url,
+							name: query.game.name
+						});
+					this.setState({
+						searchedGames: tempArray
+					})
+				}
+			})
+		}
+	};
+
+	expandChatBox = () => {
+		if (this.state.chatboxExpanded === false) {
+			this.setState({
+				chatboxExpanded: true
+			})
+		} else {
+			this.setState({
+				chatboxExpanded: false
+			})
+		}
+	};
+
+	// receive msg
+	sendMsg = (event) => {
+		event.preventDefault();
+		const {socket} = this.state;
+		//change to state later
+		let msg = document.getElementById('chatInput');
+
+		socket.emit('chat message', msg.value);
+		msg.value = "";
+
+		socket.on('chat message', (msg) => {
+			let msgP = document.createElement("p").text = msg;
+			document.getElementById('msgs').append(msgP);
+		});
 
 	};
 
-  logout = () => {
-    userAPI.logout().then(()=> {
-      //reload the window on sucessful logout
-      window.location.reload();
-    });
-  };
+	render() {
+		return (
+			<div>
 
-  //check the state of the theme toggle on the dashboard
-  switchState = () => {
-    if (this.props.theme === 1) {
-      document.getElementById("switch").checked = false;
-    } else if (this.props.theme === 2) {
-      document.getElementById("switch").checked = true;
-    } 
-  }
+				{/* chat */}
+				<Chat titleClick = {this.expandChatBox} chatExpanded = {this.state.chatboxExpanded} sendMsg = {this.sendMsg}/>
+				{/* right panel */}
+				<RightPanel closeRightPanel = {this.closeRightPanel} searchedGames = {this.state.searchedGames} gameSearch = {this.gameSearch}/>
+				{/* nav panel */}
+				<SidePanel username = {this.props.username} buttonClick = {this.logout} buttonText = {"Logout"} profileImg = {this.props.profileImg}
+					active = {this.props.active}/>
 
+				<div className = "content container-fluid">
+					<Searchbar themeChecked = {this.props.themeChecked} toggleTheme = {this.toggleTheme} openRightPanel = {this.openRightPanel}
+						closeRightPanel = {this.closeRightPanel}/>
+					{this.props.children}
+				</div>
 
-  toggleTheme = () => {
-    if (this.state.theme === 1) {
-      let data = {
-        theme: 2
-      };
-      userAPI.update(data).then(() => {
-        this.setState({
-          theme: 2
-        });
+			</div>
 
-        document.getElementById("theme-div").classList.remove("light-theme");
-        document.getElementById("theme-div").classList.add("dark-theme")
-      })
-    } else {
+		)
 
-      let data = {
-        theme: 1
-      };
-      userAPI.update(data).then(() => {
-        this.setState({
-          theme: 1
-        })
-      })
-      document.getElementById("theme-div").classList.remove("dark-theme")
-      document.getElementById("theme-div").classList.add("light-theme")
-    }
-  }
-
-  openRightPanel = () => {
-    document.getElementById("mySidenav").style.width = "900px";
-    this.setState({
-      rightPanelOpen: true,
-      searchedGames: []
-    })
-  }
-
-  closeRightPanel = () => {
-    document.getElementById("mySidenav").style.width = "0";
-    this.setState({
-      rightPanelOpen: false,
-      searchedGames: []
-    })
-  }
-
-  gameSearch = (event) => {
-    event.preventDefault();
-    if(this.state.rightPanelOpen === true){
-
-      let query = document.getElementById("gameSearch").value;
-      gamesAPI.gameSearch(query).then(data => {
-        // this.setState({
-          //   searchedGames: data
-          // })
-          console.log(data.data)
-          //data.data[0].game.cover
-          //get cover
-          if(this.state.rightPanelOpen === true){
-
-            data.data.forEach(element => {
-              
-              this.coverSearch(element)
-              
-            });
-          }
-        })
-      }
-  }
-
-  coverSearch = (query) => {
-    //need to send game name and id to put in state
-    console.log(query) ;
-    if(query.game !== undefined){
-
-        gamesAPI.gameCover(query.game.cover).then(data => {
-          let tempArray = this.state.searchedGames;
-          if(data.data[0] !== undefined){
-
-            data.data[0].url = data.data[0].url.replace('t_thumb', 't_cover_big')
-            tempArray.push(
-              {
-                id: query.game.id,
-                imgUrl:  data.data[0].url,
-                name: query.game.name
-              });
-              this.setState({
-                searchedGames: tempArray
-              })
-            }
-          })
-      }
-      }
-
-      expandChatBox = () => {
-        if(this.state.chatboxExpanded === false){
-          this.setState({
-            chatboxExpanded: true
-          })
-        } else {
-          this.setState({
-            chatboxExpanded: false
-          })
-        }
-      }
-
-      // receive msg
-      sendMsg = (event) => {
-        event.preventDefault();
-        const { socket } = this.state;
-        //change to state later
-        let msg = document.getElementById('chatInput');
-
-        socket.emit('chat message', msg.value);
-        msg.value = "";
-         
-        socket.on('chat message', (msg) => {
-          let msgP = document.createElement("p").text = msg;
-          document.getElementById('msgs').append(msgP);
-        }); 
-
-      }
-  render() {
-    return (
-      <div>
-          
-        {/* chat */}
-        <Chat titleClick={this.expandChatBox} chatExpanded={this.state.chatboxExpanded} sendMsg={this.sendMsg}/>
-        {/* right panel */}
-        <RightPanel closeRightPanel={this.closeRightPanel} searchedGames={this.state.searchedGames} gameSearch={this.gameSearch} />
-        {/* nav panel */}
-        <SidePanel username={this.props.username} buttonClick={this.logout} buttonText={"Logout"} profileImg={this.props.profileImg} active={this.props.active} />
-
-        <div className="content container-fluid">
-          <Searchbar themeChecked={this.props.themeChecked} toggleTheme={this.toggleTheme} openRightPanel={this.openRightPanel} closeRightPanel={this.closeRightPanel}/>
-          {this.props.children}
-        </div>
-
-      </div>
-
-    )
-
-  }
+	}
 }
 
 
