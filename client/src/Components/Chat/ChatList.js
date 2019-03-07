66253f4e54749a2847f0b9039dd8d578b28afae7
Bobby Boyd <bboyd2008@gmail.com>
2019-03-06 18:20:03 -0800

    fixed .length error

--- client/src/Components/Chat/ChatList.js
+++ client/src/Components/Chat/ChatList.js
@@ -1,35 +1,38 @@
 import React from "react";
 import "./Chat.css";
 const truncate = string => {
-  if (string.length > 10) return `${string.substring(0, 10)}...`;
+  if (string.length !== undefined) {
+
+    if (string.length > 10) return `${string.substring(0, 10)}...`;
+  }
   return string;
 };
 
 const ChatList = props => (
-  <div  className="chatList"  onClick={props.toggleChatDisplay}>
-   
+  <div className="chatList" onClick={props.toggleChatDisplay}>
+
     {
       props.usersChats.map(chatBox => {
-      return (
-        <div
-          className="chatDiv"
-         
-          data-chatid={chatBox._id}
-        >
-        <div>
+        return (
+          <div
+            className="chatDiv"
 
-          <h2 data-chatid={chatBox._id}>
-            {chatBox.user1 === props.username ? chatBox.user2 : chatBox.user1}
-          </h2>
-        </div>
+            data-chatid={chatBox._id}
+          >
+            <div>
+
+              <h2 data-chatid={chatBox._id}>
+                {chatBox.user1 === props.username ? chatBox.user2 : chatBox.user1}
+              </h2>
+            </div>
             {/* on the list show the name of the person you are talking to */}
             <div>
 
-            <p data-chatid={chatBox._id}>{truncate(chatBox.gameName)}</p>
+              <p data-chatid={chatBox._id}>{truncate(chatBox.gameName)}</p>
             </div>
-        </div>
-      );
-    })}
+          </div>
+        );
+      })}
   </div>
 );
 
